45ccd6b4d964ea45f0f2241a4e5b45936594e952
JordanD <dhondtjordan@hotmail.com>
2026-01-27 15:09:47 +0100

    MapMaking

--- Assets/Packages/Ticks/Runtime/TickMono_OnCollisionWithAnyFromPlayer.cs
+++ Assets/Packages/Ticks/Runtime/TickMono_OnCollisionWithAnyFromPlayer.cs
@@ -4,37 +4,39 @@ namespace Eloi.Tick
 {
 
     public class TickMono_OnCollisionWithAnyFromPlayer : TickMono_AbstractDefault
-{
+    {
 
-        [TextArea(2,10)]
+        [TextArea(2, 10)]
         public string m_contextDescription;
 
-    [Tooltip("Specify which layers should trigger the event.")]
-    public LayerMask m_layerMask = ~0;
+        [Tooltip("Specify which layers should trigger the event.")]
+        public LayerMask m_layerMask = ~0;
 
-    public bool m_useCollision = true;
-    public bool m_useTrigger = true;
+        public bool m_useCollision = true;
+        public bool m_useTrigger = true;
 
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (m_useCollision && IsInLayerMask(collision.gameObject))
+        private void OnCollisionEnter(Collision collision)
         {
-            m_onTick?.Invoke();
+            if (m_useCollision && IsInLayerMask(collision.gameObject))
+            {
+                m_onTick?.Invoke();
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (m_useTrigger && IsInLayerMask(other.gameObject))
+            {
+                m_onTick?.Invoke();
+            }
+        }
+
+        private bool IsInLayerMask(GameObject obj)
+        {
+            return (m_layerMask.value & (1 << obj.layer)) != 0;
         }
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (m_useTrigger && IsInLayerMask(other.gameObject))
-        {
-            m_onTick?.Invoke();
-        }
-    }
-
-    private bool IsInLayerMask(GameObject obj)
-    {
-        return (m_layerMask.value & (1 << obj.layer)) != 0;
-    }
-}
+   
 }
