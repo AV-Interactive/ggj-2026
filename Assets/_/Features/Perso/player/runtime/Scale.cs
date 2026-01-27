5512dd0c90d0ec9abaed436ac65285eceb1064de
mrtoine <anthony.violet@outlook.be>
2026-01-27 11:27:26 +0100

    update scripts

--- Assets/_/Features/Perso/player/runtime/Scale.cs
+++ Assets/_/Features/Perso/player/runtime/Scale.cs
@@ -6,81 +6,92 @@ namespace PlayerRunTime
     {
 
         #region Publics
-
-        [SerializeField] public Transform scaleTransfom;
-        [SerializeField] public float scaleX;
-        [SerializeField] public float scaleY;
-        [SerializeField] public float scaleZ;
-        [SerializeField] public float scaleTime = 5f;
-        [SerializeField] public float speedScale;
-        private float timer = 0f;
-        private bool isScalingDown = false;
-        private bool isScalingUp = false;
-        private float currentScaleTime = 0f;
-
-
+        [SerializeField] private Transform _scaleTransform;
+        [SerializeField] private float _scaleX;
+        [SerializeField] private float _scaleY;
+        [SerializeField] private float _scaleZ;
+        [SerializeField] private float _scaleTime = 5f;
+        [SerializeField] private float _speedScale = 1f;
+        [SerializeField] private bool _scaleDownAction;
+        [SerializeField] private bool _scaleUpAction;
         #endregion
 
 
         #region Unity API
 
-
-        void Update()
+        private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Z))
-            {
-                isScalingDown = true;
-                isScalingUp = false;
-                timer = 0f;
-                currentScaleTime = scaleTime;
-            }
-
-            if (Input.GetKeyDown(KeyCode.X))
-            {
-                isScalingUp = true;
-                isScalingDown = false;
-                timer = 0f;
-                currentScaleTime = scaleTime;
-            }
-
-            if (isScalingDown)
-            {
-                timer += Time.deltaTime * speedScale;
-                if (timer >= 1f && currentScaleTime >= 1f)
-                {
-                    scaleTransfom.localScale -= new Vector3(scaleX, scaleY, scaleZ);
-                    currentScaleTime--;
-                    timer = 0f;
-                    if (currentScaleTime < 1f)
-                    {
-                        isScalingDown = false;
-                    }
-                }
-            }
-
-            if (isScalingUp)
-            {
-                timer += Time.deltaTime * speedScale;
-                if (timer >= 1f && currentScaleTime >= 1f)
-                {
-                    scaleTransfom.localScale += new Vector3(scaleX, scaleY, scaleZ);
-                    currentScaleTime--;
-                    timer = 0f;
-                    if (currentScaleTime < 1f)
-                    {
-                        isScalingUp = false;
-                    }
-                }
-            }
+            ResetIfBothActionsCompleted();
+            ProcessScaling();
         }
-
         #endregion
 
 
         #region Main Methods
+        public void OnScaleDown(bool scaleDownAction)
+        {
+            if (!_hasScaledDown)
+            {
+                StartScaling(ref _isScalingDown, ref _isScalingUp);
+            }
+        }
 
-        // 
+        public void OnScaleUp(bool scaleUpAction)
+        {
+            if (!_hasScaledUp)
+            {
+                StartScaling(ref _isScalingUp, ref _isScalingDown);
+            }
+        }
 
+        private void StartScaling(ref bool activeScaling, ref bool inactiveScaling)
+        {
+            activeScaling = true;
+            inactiveScaling = false;
+            _timer = 0f;
+            _currentScaleTime = _scaleTime;
+        }
+
+        private void ProcessScaling()
+        {
+            if (_isScalingDown)
+            {
+                ApplyScaling(-1f, ref _isScalingDown, ref _hasScaledDown);
+            }
+
+            if (_isScalingUp)
+            {
+                ApplyScaling(1f, ref _isScalingUp, ref _hasScaledUp);
+            }
+        }
+
+        private void ApplyScaling(float direction, ref bool isScaling, ref bool hasScaled)
+        {
+            _timer += Time.deltaTime * _speedScale;
+
+            if (_timer >= 1f && _currentScaleTime >= 1f)
+            {
+                Vector3 scaleChange = new Vector3(_scaleX, _scaleY, _scaleZ) * direction;
+                _scaleTransform.localScale += scaleChange;
+                _currentScaleTime--;
+                _timer = 0f;
+
+                if (_currentScaleTime < 1f)
+                {
+                    isScaling = false;
+                    hasScaled = true;
+                }
+            }
+        }
+
+        private void ResetIfBothActionsCompleted()
+        {
+            if (_hasScaledDown && _hasScaledUp)
+            {
+                _hasScaledDown = false;
+                _hasScaledUp = false;
+            }
+        }
         #endregion
 
 
@@ -92,10 +103,12 @@ namespace PlayerRunTime
 
 
         #region Privates and Protected
-
-        // Variables privées
-
-        #endregion
+    private float _timer = 0f;
+    private bool _isScalingDown = false;
+    private bool _isScalingUp = false;
+    private float _currentScaleTime = 0f;
+    private bool _hasScaledDown = false;
+    private bool _hasScaledUp = false;
+    #endregion
     }
 }
-
